4f76181790b69bf8ad7f5809b46a4c18c34cbfe9
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2022-09-05 20:17:37 +0200

    add multiannotators arrows use tokeninfo div for labels

--- teitok-project/Scripts/linkmwes.js
+++ teitok-project/Scripts/linkmwes.js
@@ -5,12 +5,16 @@ var seq = []; var selstring = '';
 var colorlist = ['#990000', '#009900', '#000099', '#999900', '#990099', '#009999', '#990000', '#009900', '#000099', '#999900', '#990099', '#009999', '#990000', '#009900', '#000099', '#999900', '#990099', '#009999', '#990000', '#009900', '#000099', '#999900', '#990099', '#009999']
 var setcolor = [];
 
+var annotatorlist = ['','1','2']
+var annotatorlinelist = [false,{len: 2, gap: 2},{len: 6, gap: 2}]
+
 var tokinfo = document.getElementById('tokinfo');
 if ( !tokinfo ) {
-	var tokinfo = document.createElement("div"); 
+	tokinfo = document.createElement("div");
 	tokinfo.setAttribute('id', 'tokinfo');
 	document.body.appendChild(tokinfo);
 };
+tokinfo.style.width='max-content';
 
 if ( jmp ) { 
 	var it = document.getElementById(jmp);
@@ -20,10 +24,14 @@ if ( jmp ) {
 
 var linkmwescolor;
 for ( var i=0; i<Object.keys(linkmweslist).length; i++) {
+
+  for ( var j=0; j<annotatorlist.length; j++) {
+
+
 	var tmp = Object.keys(linkmweslist)[i];
 	var type_val = linkmweslist[tmp]['value'];
 	if ( !type_val ) { type_val = tmp; };
-	var its = mtxt.querySelectorAll('['+colorattr+'="'+type_val+'"]');
+	var its = mtxt.querySelectorAll('['+colorattr+'="'+type_val+annotatorlist[j]+'"]');
 	linkmwescolor = linkmweslist[tmp]['color'];
 	if ( !linkmwescolor ) { // Choose a color for this class
 		if ( !setcolor[tmp] ) { setcolor[tmp] = colorlist.shift() }; 
@@ -33,6 +41,9 @@ for ( var i=0; i<Object.keys(linkmweslist).length; i++) {
 	for ( var a = 0; a<its.length; a++ ) {
 		var it = its[a];	
 		var itcolor = linkmwescolor;
+		if(!('line' in it)){
+		  it.line = [];
+	  }
 
 
     if(it.getAttribute(linkto)){
@@ -53,6 +64,7 @@ for ( var i=0; i<Object.keys(linkmweslist).length; i++) {
     		               	  endSocket: 'top',
     		               	  endSocketGravity: 5,
     		               	  path: 'grid',
+    		               	  dash: annotatorlinelist[j]
     		               	});
     	  var representant_to = document.getElementById(item);
     	  assign_line(it,line,'start');
@@ -80,6 +92,7 @@ for ( var i=0; i<Object.keys(linkmweslist).length; i++) {
 			if ( !jmp ) { it.scrollIntoView(true); }; // TODO: this should depend on jmp
 		}
 	};
+  };
 };
 
 
@@ -122,31 +135,68 @@ function set_elem (elm, itcolor, main_elem) {
 	elm.style.borderBottomColor = itcolor;
 	elm.style.borderBottomStyle = 'solid';
 	elm.onmouseover = function(event) {
-			showinfo(main_elem);
+			showinfo(main_elem,elm);
 	};
 	elm.onmouseout = function(event) {
-			hideinfo(main_elem);
+			hideinfo(main_elem,elm);
 		};
 }
 
 
-function hideinfo(elm) {
-	elm.line.forEach(function (item) {
+function hideinfo(main_elem,elm) {
+	main_elem.line.forEach(function (item) {
     item.line.size = 1.5;
     item.line.color = 'rgba(226,0,122,0.6)';
 	  if(item.position === 'end') item.line.setOptions({endLabel:''});
     else item.line.setOptions({startLabel: ''});
   });
+  tokinfo.style.display = 'none';
 };
 
-function showinfo(elm) {
+function showinfo(main_elem,elm) {
 
-	var label = LeaderLine.captionLabel(elm.getAttribute(colorattr),{ color: elm.style.color, outlineColor: 'white'});
-	elm.line.forEach(function (item) {
+	var label = LeaderLine.captionLabel(main_elem.getAttribute(colorattr),{ color: main_elem.style.color, outlineColor: 'white'});
+	main_elem.line.forEach(function (item) {
     item.line.size = 2.5;
-    item.line.color = elm.style.color;
-	  if(item.position === 'end') item.line.setOptions({endLabel:label});
-    else item.line.setOptions({startLabel: label});
+    item.line.color = main_elem.style.color;
+	//  if(item.position === 'end') item.line.setOptions({endLabel:label});
+  //  else item.line.setOptions({startLabel: label});
   });
+  tokinfo.style.display = 'block';
+	var foffset = offset(elm);
+	tokinfo.style.left = Math.min ( foffset.left, window.innerWidth - tokinfo.offsetWidth + window.pageXOffset ) + 'px';
+	tokinfo.style.top = ( foffset.top + elm.offsetHeight + 4 ) + 'px';
+	tokinfo.style.color = main_elem.style.color;
+	tokinfo.style.fontWeight = 'bold';
+  tokinfo.innerHTML = main_elem.getAttribute(colorattr);
 
+console.log(tokinfo, elm);
+
+};
+
+
+
+
+function offset(elem) {
+if(!elem) elem = this;
+
+var x = elem.offsetLeft;
+var y = elem.offsetTop;
+
+if ( typeof(x) == "undefined" ) {
+
+bbr = elem.getBoundingClientRect();
+x = bbr.left + window.pageXOffset;
+y = bbr.top + window.pageYOffset;
+
+} else {
+
+while (elem = elem.offsetParent) {
+	x += elem.offsetLeft;
+	y += elem.offsetTop;
+}
+
+};
+
+return { left: x, top: y };
 };
